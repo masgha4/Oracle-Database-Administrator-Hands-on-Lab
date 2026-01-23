599f76e2ab56694202095b2498bef01c93fba7eb
Muhammad Wahab Asghar <93857525+masgha4@users.noreply.github.com>
2026-01-23 13:35:41 -0600

    Create verification.sql

---
+++ parameters-memory-fra/verification.sql
@@ -0,0 +1,16 @@
+SHOW PARAMETER sga_target;
+SHOW PARAMETER sga_max_size;
+SHOW PARAMETER pga_aggregate_target;
+SHOW PARAMETER sessions;
+SHOW PARAMETER processes;
+
+SHOW PARAMETER spfile;
+SHOW PARAMETER pfile;
+
+SHOW PARAMETER db_recovery_file_dest_size;
+
+SHOW PARAMETER sort_area_size;
+SHOW PARAMETER shared_pool_reserved_size;
+SHOW PARAMETER db_block_size;
+SHOW PARAMETER java_pool_size;
+SHOW PARAMETER session_cached_cursors;
